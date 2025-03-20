d0d58b063619099a92abb8e9998933cc94912e41
Yushen Zuo <zuoyushen12@gmail.com>
2025-03-20 17:58:59 +0800

    update setup

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@ CUDA_VISIBLE_DEVICES=1 python test_ugcsr.py \
     --pretrained_fusion_path pretrained_models/fusion/fusion_model.pth \
     --fusion_tile 1024 \
     --fusion_tile_overlap 64 \
-    --input_image /home/data1/NTIRE2025/ShortformUGCSR/synthetic_val_LR/LR \
-    --output_dir /home/data1/NTIRE2025/ShortformUGCSR/submissions/PISASR_pretrained_wavelet_align_fusion/synthetic
+    --input_image /home/data1/NTIRE2025/ShortformUGCSR/synthetic_test_LR/LR \
+    --output_dir /home/data1/NTIRE2025/ShortformUGCSR/submissions_test/PISASR_pretrained_wavelet_align_fusion/synthetic
 
 
 
-CUDA_VISIBLE_DEVICES=0 python test_ugcsr.py \
+CUDA_VISIBLE_DEVICES=1 python test_ugcsr.py \
     --pretrained_model_path /home/vip2024/zys/Agentic-Restoration/executor/super_resolution/tools/OSEDiff/pretrained_models/SD/stable-diffusion-2-1-base \
     --pretrained_path pretrained_models/pisa_sr.pkl \
     --pretrained_fusion_path pretrained_models/fusion/fusion_model.pth \
     --fusion_tile 1024 \
     --fusion_tile_overlap 64 \
-    --input_image /home/data1/NTIRE2025/ShortformUGCSR/wild_val \
-    --output_dir /home/data1/NTIRE2025/ShortformUGCSR/submissions/test_wild
+    --input_image /home/data1/NTIRE2025/ShortformUGCSR/wild_test \
+    --output_dir /home/data1/NTIRE2025/ShortformUGCSR/submissions_test/PISASR_pretrained_wavelet_align_fusion/wild
